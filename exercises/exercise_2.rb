0245a4265c8769d7858f9ac6c2ecebe09a0e44ec
S-Marks <marks.cherie.art@gmail.com>
2019-07-09 21:10:22 +0000

    filter mens and women's stores

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -6,7 +6,11 @@ puts "----------"
 
 # Your code goes here ...
 @store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
 puts @store1.name
+puts @store2.name
+
 @store1.name = "Hot Topic"
 @store1.save
 puts @store1.name
